4b2cc632b387de37a6477331699cebea14f95cbd
= <sdkauk@gmail.com>
2021-10-28 13:50:35 -0500

    added jump animations

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+
 public class PlayerJump : MonoBehaviour
 {
 
@@ -20,14 +23,18 @@ public class PlayerJump : MonoBehaviour
 
     [Header("Components")]
     private Rigidbody2D rb;
+    private Animator myAnimator;
+
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent< Animator >();
         jumpTimeCounter = jumpTime;
 
     }
+    
 
     private void Update()
     {
@@ -38,7 +45,11 @@ public class PlayerJump : MonoBehaviour
         if (grounded)
         {
             jumpTimeCounter = jumpTime;
+            myAnimator.ResetTrigger("jump");
+            myAnimator.SetBool("falling", false);
         }
+
+
 
         //if we press the jump button
         if (Input.GetButtonDown("Jump") && grounded)
@@ -46,6 +57,7 @@ public class PlayerJump : MonoBehaviour
             //jump:
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             stoppedJumping = false;
+            myAnimator.SetTrigger("jump");
         }
 
         //if we hold jump button
@@ -55,6 +67,7 @@ public class PlayerJump : MonoBehaviour
             //jump
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpTimeCounter -= Time.deltaTime;
+            myAnimator.SetTrigger("jump");
         }
 
         //if we release the jump button
@@ -62,11 +75,38 @@ public class PlayerJump : MonoBehaviour
         {
             jumpTimeCounter = 0;
             stoppedJumping = true;
+            myAnimator.SetBool("falling", true);
+            myAnimator.ResetTrigger("jump");
+        }
+
+        //if y velocity is below 0, you are falling.
+        if (rb.velocity.y < 0)
+        {
+            myAnimator.SetBool("falling", true);
+
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(groundcheck.position, radOCircle);
+    }
+
+    private void FixedUpdate()
+    {
+        HandleLayers();
+    }
+
+    private void HandleLayers()
+    {
+        if (!grounded)
+        {
+            myAnimator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+
+            myAnimator.SetLayerWeight(1, 0);
+        }
     }
 }
