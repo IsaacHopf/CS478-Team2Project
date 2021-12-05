d0989e3f6c8e6e2594a08791ee3e91a40790258c
IsaacHopf <iplazma3@icloud.com>
2021-12-05 16:35:56 -0500

    Added story to intro animation

--- Assets/Scripts/UI/IntroAnimation.cs
+++ Assets/Scripts/UI/IntroAnimation.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class IntroAnimationData //this data is saved during a play session, and it can be accessed from any scene
@@ -8,8 +9,14 @@ public class IntroAnimationData //this data is saved during a play session, and 
 
 public class IntroAnimation : MonoBehaviour
 {
-    [SerializeField] private GameObject playButton, exitButton, background;
-    [SerializeField] private Animator nameTextAnimator, theDemonSlayerTextAnimator, fadeOutIdleLadyAnimator, fadeInIdleHeroineAnimator;
+    //Necessary game objects and components
+    [SerializeField] 
+    private GameObject playButton, exitButton, background;
+    [SerializeField] 
+    private Animator fadeInNameTextAnimator, fadeInTheDemonSlayerTextAnimator, fadeOutIdleLadyAnimator, fadeInIdleHeroineAnimator, introAnimationTextAnimator, fadeOutBlackBackgroundAnimator;
+    [SerializeField] 
+    private TextMeshProUGUI introAnimationText;
+
     private Animator introAnimator;
 
     private void Start()
@@ -30,10 +37,10 @@ public class IntroAnimation : MonoBehaviour
     private IEnumerator PlayIntroAnimation()
     {
         introAnimator.SetTrigger("PlayIntro");
-        yield return new WaitForSeconds(52);
-        nameTextAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(63f);
+        fadeInNameTextAnimator.SetTrigger("Start");
         yield return new WaitForSeconds(2f);
-        theDemonSlayerTextAnimator.SetTrigger("Start");
+        fadeInTheDemonSlayerTextAnimator.SetTrigger("Start");
         yield return new WaitForSeconds(2.5f);
         playButton.SetActive(true);
         exitButton.SetActive(true);
@@ -41,16 +48,19 @@ public class IntroAnimation : MonoBehaviour
 
     private IEnumerator LoadMainMenu()
     {
+        fadeOutBlackBackgroundAnimator.gameObject.SetActive(false);
+
         background.SetActive(true);
         yield return new WaitForSeconds(1f);
-        nameTextAnimator.SetTrigger("Start");
+        fadeInNameTextAnimator.SetTrigger("Start");
         yield return new WaitForSeconds(1f);
-        theDemonSlayerTextAnimator.SetTrigger("Start");
+        fadeInTheDemonSlayerTextAnimator.SetTrigger("Start");
         yield return new WaitForSeconds(1.5f);
         playButton.SetActive(true);
         exitButton.SetActive(true);
     }
 
+    //Functions called during IntroAnimation
     private void StartFadeOutIdleLady()
     {
         fadeOutIdleLadyAnimator.SetTrigger("Start");
@@ -59,5 +69,42 @@ public class IntroAnimation : MonoBehaviour
     private void StartFadeInIdleHeroine()
     {
         fadeInIdleHeroineAnimator.SetTrigger("Start");
+    }
+
+    private void StartFadeOutBlackBackground()
+    {
+        fadeOutBlackBackgroundAnimator.SetTrigger("Start");
+    }
+
+    private int storyIndex = 0;
+    private string[] storyText = {"Long ago ...",
+        "Demons infested the Outer Lands ...",
+        "until the world's hero fought back.",
+        "He was beloved by many ...",
+        "including his true love, Rose.",
+        "But the demons returned ...",
+        "and captured the world's hero.",
+        "Rose vowed to end the demons.",
+        "Will Rose save the world ...",
+        "and her true love?"};
+
+    private IEnumerator ShowNextIntroAnimationTextShort()
+    {
+        introAnimationText.text = storyText[storyIndex];
+        introAnimationTextAnimator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(2.5f);
+        introAnimationTextAnimator.SetTrigger("FadeOut");
+
+        storyIndex++;
+    }
+
+    private IEnumerator ShowNextIntroAnimationTextLong()
+    {
+        introAnimationText.text = storyText[storyIndex];
+        introAnimationTextAnimator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(3.5f);
+        introAnimationTextAnimator.SetTrigger("FadeOut");
+
+        storyIndex++;
     }
 }
